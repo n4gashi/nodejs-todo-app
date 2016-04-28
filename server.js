66cdd6d870b95fa290f87f81bd60e208d70693c2
Eric Martin <eric.iLife.pro@gmail.com>
2016-04-28 13:48:41 -0700

    Add DELETE todos route

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/todos/:id', function(req, res) {
 
 });
 
-app.post('/todos', function(req, res){
+app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
@@ -47,6 +47,19 @@ app.post('/todos', function(req, res){
 	todos.push(body);
 
 	res.json(body);
+});
+
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo) {
+		res.status(404).json({"error": "No todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+
 });
 
 app.listen(PORT, function() {
