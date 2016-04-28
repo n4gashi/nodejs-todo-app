ba37897a553f27d14f369c40668c353d8d8fcc89
Eric Martin <eric.iLife.pro@gmail.com>
2016-04-28 14:33:53 -0700

    Add PUT todos

--- server.js
+++ server.js
@@ -49,6 +49,38 @@ app.post('/todos', function(req, res) {
 	res.json(body);
 });
 
+app.put('/todos/:id', function(req, res) {
+	
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	// No todo with this id
+	if(!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	// Validate completed
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	// Validate description
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	// Update matchedTodo
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
+});
+
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
